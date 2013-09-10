606bf9f87d2d8a274faa752a0b3fdc273bda816e
Chase Zhang <yun.er.run@gmail.com>
2013-09-10 22:31:15 +0800

    init xcode project (with gitignore)

---
+++ bell/bell/bellPlayer.h
@@ -0,0 +1,13 @@
+//
+//  bellPlayer.h
+//  bell
+//
+//  Created by AnakinGWY on 9/9/13.
+//  Copyright (c) 2013 xiuxiude. All rights reserved.
+//
+
+#import <AVFoundation/AVFoundation.h>
+
+@interface bellPlayer : AVAudioPlayer
+
+@end
---
+++ bell/bell/bellPlayer.m
@@ -0,0 +1,13 @@
+//
+//  bellPlayer.m
+//  bell
+//
+//  Created by AnakinGWY on 9/9/13.
+//  Copyright (c) 2013 xiuxiude. All rights reserved.
+//
+
+#import "bellPlayer.h"
+
+@implementation bellPlayer
+
+@end
