8a67e7688b28e35e46656f47827924230d60c1fb
Chase Zhang <yun.er.run@gmail.com>
2013-09-14 00:30:27 +0800

    Basic frameworks of BellMacDemo

---
+++ Demo/BellMacDemo/BellMacDemo/BEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BEAppDelegate.h
+//  BellMacDemo
+//
+//  Created by Chase Zhang on 9/13/13.
+//  Copyright (c) 2013 diumoo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BEAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Demo/BellMacDemo/BellMacDemoTests/BellMacDemoTests.m
@@ -0,0 +1,32 @@
+//
+//  BellMacDemoTests.m
+//  BellMacDemoTests
+//
+//  Created by Chase Zhang on 9/13/13.
+//  Copyright (c) 2013 diumoo. All rights reserved.
+//
+
+#import "BellMacDemoTests.h"
+
+@implementation BellMacDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BellMacDemoTests");
+}
+
+@end
