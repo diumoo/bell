a18723145e8b92193a65d55be9656a7fe1d4a357
Chase Zhang <yun.er.run@gmail.com>
2013-09-17 21:10:26 +0800

    Fix bugs and finished the simple demo of osx

--- BellDemo/BellDemo/BellAppDelegate.m
+++ BellDemo/BellDemo/BellAppDelegate.m
@@ -7,12 +7,43 @@
 //
 
 #import "BellAppDelegate.h"
+#import "BellPlayer.h"
 
 @implementation BellAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-  // Insert code here to initialize your application
+  
+}
+
+- (NSNumber *) fadingTimeDuration
+{
+  return @([BellPlayer sharedPlayer].fadingTimeDuration);
+}
+
+- (void) setFadingTimeDuration:(NSNumber *)fadingTimeDuration
+{
+  [BellPlayer sharedPlayer].fadingTimeDuration = [fadingTimeDuration doubleValue];
+}
+
+- (void) buttonAction:(id)sender
+{
+  switch ([sender tag]) {
+    case 0:
+    {
+      NSString *urlString = self.audioUrlField.stringValue;
+      NSURL *url = [NSURL URLWithString:urlString];
+      [[BellPlayer sharedPlayer] playURL:url];
+    }
+      break;
+    case 1:
+      [[BellPlayer sharedPlayer] play];
+      break;
+    case 2 :
+      [[BellPlayer sharedPlayer] pause];
+    default:
+      break;
+  }
 }
 
 @end
